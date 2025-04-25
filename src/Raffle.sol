1fffb517c03c15ebc90cf2a132d930844218224e
shaharin <ahmedshaharin7589@gmail.com>
2025-04-25 16:48:30 +0600

    feat: init lottery

---
+++ src/Raffle.sol
@@ -0,0 +1,70 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Shaharin Ahmed
+ * @notice This contract is for creating a simple raffle
+ * @dev Implments Chainlink VRFv2.5
+ */
+contract Raffle{
+    /** Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 entranceFee,uint256 interval){
+        i_entranceFee=entranceFee;
+        i_interval=interval;
+        s_lastTimeStamp=block.timestamp;
+    }
+
+    function enterRaffle() external payable{
+         // require(msg.value >= i_entranceFee, "Not enough value sent");
+        // require(s_raffleState == RaffleState.OPEN, "Raffle is not open");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
+
+    function pickWinner() external{
+        // check to see if enough time has passed
+        if((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
+    /** Getter functions */
+    function getEntranceFee() external view returns(uint256){
+        return i_entranceFee;
+    }
+}
